91f57ea477539de9adf7ecac318147c5598f43bf
Maciej Swiderski <swiderski.maciej@gmail.com>
2020-01-22 13:52:48 -0500

    initial example with business app using kjars as dependencies

---
+++ business-application-service/src/main/java/com/company/service/Application.java
@@ -0,0 +1,31 @@
+package com.company.service;
+
+import java.util.Arrays;
+
+import org.kie.server.api.KieServerConstants;
+import org.kie.server.api.model.KieContainerResource;
+import org.kie.server.api.model.KieServerConfigItem;
+import org.kie.server.api.model.ReleaseId;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    KieContainerResource evaluation() {
+        return new KieContainerResource("evaluation", new ReleaseId("evaluation", "evaluation", "1.0"));
+    }
+    
+    @Bean
+    KieContainerResource evaluation2() {
+        KieContainerResource container = new KieContainerResource("evaluation2", new ReleaseId("evaluation2", "evaluation2", "2.0"));
+        container.setConfigItems(Arrays.asList(new KieServerConfigItem(KieServerConstants.PCFG_RUNTIME_STRATEGY, "PER_PROCESS_INSTANCE", "String")));
+        return container;
+    }
+}
